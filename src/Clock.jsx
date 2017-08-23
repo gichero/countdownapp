5314d41a4b26f413a7b209c5cd51ba46319a4178
Amos <amosgathungu@gmail.com>
2017-08-22 22:50:14 -0400

    styling with react-bootstrap

--- src/Clock.jsx
+++ src/Clock.jsx
@@ -17,6 +17,14 @@ class Clock extends Component {
     componentDidMount(){
         setInterval(() => this.getTimeCountdown(this.props.deadline), 1000)
     }
+    leadingZero(num){
+        // if (num < 10){
+        //     return '0' + num;
+        // }
+        // return num;
+
+        return num < 10 ? '0' + num : num;
+    }
 
     getTimeCountdown(deadline){
         const time = Date.parse(deadline) - Date.parse(new Date());
@@ -33,11 +41,11 @@ class Clock extends Component {
 
         return(
             <div>
-                <div className = 'Clock-days'>{this.state.days} days</div>
-                <div className = 'Clock-hours'>{this.state.hours} hours</div>
-                <div className = 'Clock-minutes'>{this.state.minutes} minutes
+                <div className = 'Clock-days'>{this.leadingZero(this.state.days)} days</div>
+                <div className = 'Clock-hours'>{this.leadingZero(this.state.hours)} hours</div>
+                <div className = 'Clock-minutes'>{this.leadingZero(this.state.minutes)} minutes
                 </div>
-                <div className = 'Clock-seconds'>{this.state.seconds} seconds
+                <div className = 'Clock-seconds'>{this.leadingZero(this.state.seconds)} seconds
                 </div>
             </div>
         )
